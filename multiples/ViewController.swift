be8dfa59ff618e21d7e0c7f0b4fe05be558becd8
Omair Javed <omairj7@gmail.com>
2016-02-15 22:59:43 -0500

    Working version of game

--- multiples/ViewController.swift
+++ multiples/ViewController.swift
@@ -10,15 +10,68 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    // properties
+    var maxSum: Int = 40
+    var currSum: Int = 0
+    var multipleAdd: Int = 0
+    
+    // outlets
+    @IBOutlet weak var logo: UIImageView!
+    @IBOutlet weak var multiplesEntryText: UITextField!
+    @IBOutlet weak var playBtn: UIButton!
+    
+    @IBOutlet weak var mathLbl: UILabel!
+    @IBOutlet weak var addBtn: UIButton!
+    
+    // actions
+    
+    // starts multiple game by showing/hiding appropriate views
+    @IBAction func onPlayButtonPressed(sender: UIButton!){
+        if multiplesEntryText.text != nil && multiplesEntryText.text != "" {
+            logo.hidden = true
+            multiplesEntryText.hidden = true
+            playBtn.hidden = true
+            
+            mathLbl.hidden = false
+            addBtn.hidden = false
+            
+            currSum = 0
+            multipleAdd = Int(multiplesEntryText.text!)!
+            
+            mathLbl.text = "Press Add to add!"
+            
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    
+    // adds multiple to current sum and checks if game finished
+    @IBAction func onAddButtonPressed(sender: UIButton!) {
+        currSum += multipleAdd
+        updateMathLabel()
+        
+        if(currSum >= maxSum) {
+            restartGame();
+        }
     }
+    
+    func updateMathLabel() {
+        mathLbl.text = "\(currSum - multipleAdd) + \(multipleAdd) = \(currSum)"
+    }
+    
+    func restartGame() {
+        logo.hidden = false
+        multiplesEntryText.hidden = false
+        playBtn.hidden = false
+        
+        mathLbl.hidden = true
+        addBtn.hidden = true
+        
+        multiplesEntryText.text = ""
+    }
+    
+    
+    
+    
 
 
 }
